698cbb1915605f8b4aaaf69268de75a9098d3f6a
Ken M <kegmor07@gmail.com>
2026-02-27 10:46:34 -0600

    created a metrics counter and handler for site visits

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sync/atomic"
 )
 
 type Server struct {
@@ -10,16 +11,23 @@ type Server struct {
 	Handler *http.ServeMux
 }
 
+type apiConfig struct {
+	fileserverHits atomic.Int32
+	Handler        *http.ServeMux
+}
+
 func main() {
+	var apiCfg apiConfig
 	mux := http.NewServeMux()
 	server := &Server{
 		Addr:    ":8080",
 		Handler: mux,
 	}
 	server.Handler.HandleFunc("/healthz/", server.ServeHTTP)
+	server.Handler.HandleFunc("/metrics/", apiCfg.middlewareHits)
 	server.Handler.Handle(
 		"/app/",
-		http.StripPrefix("/app", http.FileServer(http.Dir("."))),
+		http.StripPrefix("/app", apiCfg.middlewareMetricsInc(http.FileServer(http.Dir(".")))),
 	)
 	err := http.ListenAndServe(server.Addr, server.Handler)
 	if err != nil {
@@ -32,5 +40,21 @@ func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	_, err := w.Write([]byte("OK"))
 	if err != nil {
 		fmt.Println("failed to write response")
+	}
+}
+
+func (api *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		api.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (api *apiConfig) middlewareHits(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	_, err := fmt.Fprintf(w, "Hits: %d\n", api.fileserverHits.Load())
+	if err != nil {
+		fmt.Println("failed to write response counter")
 	}
 }
