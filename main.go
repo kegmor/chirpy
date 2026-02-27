40ca2d6735fef2bff7c5de28343d2836f63df419
Ken M <kegmor07@gmail.com>
2026-02-27 09:04:26 -0600

    set up new server

--- main.go
+++ main.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type Server struct {
+	Addr    string
+	Handler http.Handler
+}
+
+func main() {
+	server := Server{Addr: ":8080"}
+	mux := http.NewServeMux()
+	err := http.ListenAndServe(server.Addr, mux)
+	if err != nil {
+		fmt.Println("failed to start server")
+	}
+}
