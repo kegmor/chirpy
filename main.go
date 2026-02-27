947b834a127a34cf3e27ff278705fa362629eaa0
Ken M <kegmor07@gmail.com>
2026-02-27 10:28:11 -0600

    updated routing and added a readiness endpoint

--- main.go
+++ main.go
@@ -16,9 +16,21 @@ func main() {
 		Addr:    ":8080",
 		Handler: mux,
 	}
-	server.Handler.Handle("/", http.FileServer(http.Dir(".")))
+	server.Handler.HandleFunc("/healthz/", server.ServeHTTP)
+	server.Handler.Handle(
+		"/app/",
+		http.StripPrefix("/app", http.FileServer(http.Dir("."))),
+	)
 	err := http.ListenAndServe(server.Addr, server.Handler)
 	if err != nil {
 		fmt.Println("failed to start server")
+	}
+}
+func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	_, err := w.Write([]byte("OK"))
+	if err != nil {
+		fmt.Println("failed to write response")
 	}
 }
