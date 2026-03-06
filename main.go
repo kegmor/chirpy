a2b80e2b43c8596d8e04561fc6e2e5e85fb3c631
Ken M <kegmor07@gmail.com>
2026-03-06 11:52:52 -0600

    added metric handlers and uptime handler as well as initial databse creation

--- main.go
+++ main.go
@@ -1,43 +1,43 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	"strings"
 	"sync/atomic"
 )
 
-type Server struct {
-	Addr    string
-	Handler *http.ServeMux
-}
-
 type apiConfig struct {
 	fileserverHits atomic.Int32
-	Handler        *http.ServeMux
 }
 
 func main() {
-	var apiCfg apiConfig
+	const filePathRoot = "."
+	const port = "8080"
+	apiCfg := &apiConfig{}
 	mux := http.NewServeMux()
-	server := &Server{
-		Addr:    ":8080",
+
+	mux.HandleFunc("GET /api/healthz/", handlerReadiness)
+	mux.HandleFunc("POST /api/validate_chirp/", handlerResponse)
+	mux.HandleFunc("GET /admin/metrics/", apiCfg.handlerMetrics)
+	mux.HandleFunc("POST /admin/reset/", apiCfg.handlerReset)
+	mux.Handle(
+		"/app/",
+		apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filePathRoot)))),
+	)
+	server := &http.Server{
+		Addr:    ":" + port,
 		Handler: mux,
 	}
-	server.Handler.HandleFunc("/healthz/", server.ServeHTTP)
-	server.Handler.HandleFunc("/metrics/", apiCfg.middlewareHits)
-	server.Handler.Handle(
-		"/app/",
-		http.StripPrefix("/app", apiCfg.middlewareMetricsInc(http.FileServer(http.Dir(".")))),
-	)
-	err := http.ListenAndServe(server.Addr, server.Handler)
-	if err != nil {
-		fmt.Println("failed to start server")
-	}
+	log.Fatal(server.ListenAndServe())
+
 }
-func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-	_, err := w.Write([]byte("OK"))
+	_, err := w.Write([]byte(http.StatusText(http.StatusOK)))
 	if err != nil {
 		fmt.Println("failed to write response")
 	}
@@ -50,11 +50,75 @@ func (api *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 	})
 }
 
-func (api *apiConfig) middlewareHits(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+func (api *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-	_, err := fmt.Fprintf(w, "Hits: %d\n", api.fileserverHits.Load())
+	_, err := fmt.Fprintf(w, `<html>
+					<body>
+						<h1>Welcome, Chirpy Admin</h1>
+						<p>Chirpy has been visited %d times!</p>
+					</body>
+				</html>`, api.fileserverHits.Load())
 	if err != nil {
 		fmt.Println("failed to write response counter")
 	}
+}
+
+func (api *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	api.fileserverHits.Store(0)
+	_, err := fmt.Fprintf(w, "Hits: %d\n", api.fileserverHits.Load())
+	if err != nil {
+		fmt.Println("failed to reset counter")
+	}
+}
+
+func handlerResponse(w http.ResponseWriter, r *http.Request) {
+
+	type requestBody struct {
+		Body string `json:"body"`
+	}
+	type errorResponse struct {
+		Error string `json:"error"`
+	}
+	type validResponse struct {
+		CleanedBody string `json:"cleaned_body"`
+	}
+
+	var body requestBody
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&body)
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(errorResponse{Error: "Something went wrong"})
+		return
+	}
+
+	if len(body.Body) > 140 {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(errorResponse{Error: "Chirp is too long"})
+		return
+	}
+
+	words := replaceBadWords(body.Body)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(validResponse{CleanedBody: words})
+}
+
+func replaceBadWords(s string) string {
+	badWords := []string{"kerfuffle", "sharbert", "fornax"}
+	words := strings.Split(s, " ")
+	for i, word := range words {
+		for _, badWord := range badWords {
+			if strings.ToLower(word) == badWord {
+				words[i] = "****"
+			}
+		}
+	}
+	return strings.Join(words, " ")
 }
